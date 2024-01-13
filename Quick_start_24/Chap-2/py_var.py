7f1e58e1424736ce2c3cdcfb45fdc00fb3f42d40
Yogi Kapoor <yogi_kapoor@hotmail.com>
2024-01-13 14:00:19 -0500

    2024 Quick start

---
+++ Quick_start_24/Chap-2/py_var.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+x = 2
+print(f'{x}')
+print(x)
+print(' Here is the X variable Value: {0}' .format(23) )
+#del(x)
+print(x)
+print(type(x))
+
+x_bool = True
+y_bool = False
+print (x_bool, type(x_bool), y_bool, type(y_bool))
---
+++ Quick_start_24/multiple_var_print.py
@@ -0,0 +1,5 @@
+x = 3;y = 5.7; lang_name = "Python Scripting"
+print( x, y, lang_name)
+print("{0}\n {2}\n {1}\n" .format(lang_name,x,y))
+my_output = f"X value is: {x} \n The Y value is: {y} \n The language is: {lang_name}"
+print(my_output)
